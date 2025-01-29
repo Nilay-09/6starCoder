274a70e528b6282c234afab439921d844d3653fa
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2025-01-29 15:55:38 +0000

    Time: 0 ms (100%), Space: 12.8 MB (93.68%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
-    bitset<1001> viz;
-    vector<vector<short>> adj;
-    vector<int> parent;
-    int found = -1;
-
-    void dfs(int i) {
-        viz[i] = 1;
-        for (int j : adj[i]) {
-            if (!viz[j]) {
-                parent[j] = i;
-                dfs(j);
-            } else if (j != parent[i] && found == -1) {
-                found = j;
-                parent[j] = i;
-            }
-        }
+    int find(vector<int> &parent,int x){
+    
+    if(parent[x]==x){
+        return x;
     }
+   
+    return parent[x]=find(parent,parent[x]);
+}
 
+bool Union(vector<int>&parent,vector<int>&size,int a,int b){
+    int pa=find(parent,a);
+    int pb=find(parent,b);
+    if(pa==pb){
+        return true;
+    }
+    if(size[pa]>=size[pb]){
+      
+        size[pa]+=size[pb];
+        parent[pb]=pa;
+       
+    }
+    else{
+       
+        size[pb]+=size[pa];
+        parent[pa]=pb;
+        
+
+    }
+    return false;
+}
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        adj.resize(n + 1);
-        parent.assign(n + 1, -1);
+        int n=edges.size();
+        vector<int> parent(n+1); 
 
-        for (auto& e : edges) {
-            short u = e[0], v = e[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        dfs(edges[0][0]);
-        //    cout<<found<<endl;
-
-        bitset<1001> cycle = 0;
-        int j = found;
-        do {
-            cycle[j] = 1;
-            j = parent[j];
-        } while (j != found);
-
-        for (int i = n - 1; i >= 0; i--) {
-            int u = edges[i][0], v = edges[i][1];
-            if (cycle[u] && cycle[v])
-                return edges[i];
-        }
-        return {};
+vector<int> size(n+1,1);
+for(int i=0;i<=n;i++){
+    parent[i]=i;
+   
+}
+for(int i=0;i<n;i++){
+    if(Union(parent,size,edges[i][0],edges[i][1])){
+        return {edges[i][0],edges[i][1]};
+    }
+}
+return {};
     }
 };
