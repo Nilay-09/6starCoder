ae29a77cef8fecc58633648ccf6bdc66c2e91b04
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2025-02-01 14:36:48 +0000

    Time: 0 ms (100%), Space: 27.8 MB (25%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-
-        int n=nums.size();
-
-        bool lastParity=(nums[0]&1);
-        bool ans=1;
-        if(n==1) return true;
-        if(n==2){
-            if((nums[0] &1) == (nums[1] &1)) return false;
-            else return true;
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if ((nums[i] & 1) == (nums[i - 1] & 1)) 
+                return false;
         }
-        for(int i=1;i<n;i++){
-            if(lastParity==(nums[i]&1)) return false;
-            else lastParity=(nums[i]&1); 
-        }
-        return ans;
-        
+        return true;
     }
 };
