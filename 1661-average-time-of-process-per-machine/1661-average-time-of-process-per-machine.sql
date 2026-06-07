8f8ad383ccb1d897183e3ca974f9b59d3292f73b
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2026-06-07 18:40:02 +0530

    Time: 176 ms (37.25%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,8 +1,13 @@
--- Write your PostgreSQL query statement below
-select a.machine_id,ROUND(AVG(b.timestamp-a.timestamp)::numeric,3) as processing_time 
-from Activity a JOIN Activity b ON a.machine_id=b.machine_id
-where a.activity_type ='start' AND b.activity_type ='end'
-group by a.machine_id
+SELECT
+    a.machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp)::numeric, 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+    ON a.machine_id = b.machine_id
+   AND a.process_id = b.process_id
+   AND b.activity_type = 'end'
+   AND a.activity_type = 'start'
+GROUP BY a.machine_id;
 
 /*
 Synced seamlessly with LeetHub Pro
