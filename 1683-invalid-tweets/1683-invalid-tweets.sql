007c9d876d5b1e404ffee15b5fc2521eec88539e
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2026-06-07 14:34:19 +0530

    Time: 367 ms (41.08%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement belo
+select tweet_id from Tweets
+where LENGTH(content)>15
+
+/*
+Synced seamlessly with LeetHub Pro
+Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+*/
