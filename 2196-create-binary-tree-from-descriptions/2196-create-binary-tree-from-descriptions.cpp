f533a772c1e8bdf4da3a771602f215ad69d32433
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2026-06-07 13:49:02 +0530

    Time: 87 ms (93.3%), Space: 255.6 MB (93.45%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& A) {
+        unordered_map<int, TreeNode*> nodes;
+        nodes.reserve(A.size() + 1);
+        int root = 0;
+
+        for (auto& d : A) {
+            int x = d[0], y = d[1], isLeft = d[2];
+            if (!nodes.contains(x)) {
+                nodes[x] = new TreeNode(x);
+                root ^= x;
+            }
+            if (!nodes.contains(y)) {
+                nodes[y] = new TreeNode(y);
+                root ^= y;
+            }
+
+            if (isLeft)
+                nodes[x]->left = nodes[y];
+            else
+                nodes[x]->right = nodes[y];
+            root ^= y;
+        }
+
+        return nodes[root];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
