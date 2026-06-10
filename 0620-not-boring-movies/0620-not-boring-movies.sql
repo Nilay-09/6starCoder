d89b94a35cded4cef27de1261a7c6c0f364046af
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2026-06-10 21:04:03 +0530

    Time: 187 ms (55.19%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+select * from Cinema where id%2 !=0 AND description !='boring'
+order by rating DESC ;
+
+/*
+Synced seamlessly with LeetHub Pro
+Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+*/
