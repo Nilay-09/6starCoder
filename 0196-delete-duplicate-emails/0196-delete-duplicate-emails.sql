8f02f8bc69ac87bc5f93824b46eceff159176d0d
Nilay Bhotmange <84323867+Nilay-09@users.noreply.github.com>
2026-07-19 10:33:11 +0000

    Time: 227 ms (83.88%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,12 @@
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
+);
+
+/*
+Synced seamlessly with LeetHub Pro
+Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+*/
